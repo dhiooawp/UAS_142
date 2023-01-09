733de1fc91f608ec2af95514f5f6f76be677ad0a
DhioAWP <dhioanugerah0@gmail.com>
2023-01-09 18:09:51 +0700

    Add project files.

---
+++ UAS_142/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_142
+{
+    class Node
+    {
+        public int NoInduk;
+        public string name;
+        public Node next;
+
+    }
+    class list
+    {
+        Node START;
+        public list()
+        {
+            START = null;
+        }
+        public void addNote()
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.NoInduk = rollNo;
+            newnode.name = nm;
+
+            if (START == null || rollNo <= START.NoInduk)
+            {
+                if ((START != null) && (rollNo == START.NoInduk))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (rollNo >= current.NoInduk))
+            {
+                if (rollNo == current.NoInduk)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            newnode.next = current;
+            previous.next = newnode;
+        }
+        public bool delnode(int rollNo)
+        {
+            Node previous, current;
+            previous = current = null;
+            if (Search(rollNo, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            while ((current != null) && (rollNo != current.NoInduk))
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (current == null)
+                return false;
+            else
+                return true;
+        }
+        public void Traverse()
+        {
+            if (ListEmpty())
+                Console.WriteLine("\nThe records in the list are: ");
+            else
+            {
+                Console.WriteLine("\n The records in the list are: ");
+                Node currentNode;
+                for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    Console.Write(currentNode.NoInduk + " " + currentNode.name + "\n");
+                Console.WriteLine();
+            }
+        }
+        public bool ListEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            list obj = new list();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMENU");
+                    Console.WriteLine("1. ADD A RECORD TO THE LIST");
+                    Console.WriteLine("2. DELETE A RECORD FROM THE LIST");
+                    Console.WriteLine("3. VIEW ALL THE RECORDS IN THE LIST");
+                    Console.WriteLine("4. SEARCH A RECORD IN THE LIST");
+                    Console.WriteLine("5. EXIT");
+                    Console.WriteLine("\nEnter your choice (1-5) : ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNote();
+                            }
+                            break;
+
+                        case '2':
+                            {
+                                if (obj.ListEmpty())
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Console.WriteLine("Enter the roll number of" + "the student whose record is to be deleted: ");
+                                int rollNo = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine();
+                                if (obj.delnode(rollNo) == false)
+                                    Console.WriteLine("\n Record not found.");
+                                else
+                                    Console.WriteLine("Record with roll number" + rollNo + " Deleted");
+                            }
+                            break;
+                        case '3':
+                            {
+                                obj.Traverse();
+
+                            }
+                            break;
+                        case '4':
+                            {
+                                if (obj.ListEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node previous, current;
+                                previous = current = null;
+                                Console.Write("\nEnter the roll number of the" + "Student whole record to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref previous, ref current) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord not found");
+                                    Console.WriteLine("\nRoll number: " + current.NoInduk);
+                                    Console.WriteLine("\nName: " + current.name);
+                                }
+                            }
+                            break;
+                        case '5':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid Option");
+                                break;
+                            }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nCheck for the value enterd");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+//2. SinglyLinkedList.
+//3. TOP adalah Data yang masuk diawal atau jalur yang digunakan data untuk masuk.
+//4. ditambahkan diakhir adalah REAR dan data yang dihapus terakhir adalah FRONT.
+//5a. 5.
+//5b. Inorder = 15, 20, 25, 30, 32, 31, 35, 48, 50, 70, 65, 67, 66, 69, 90, 98, 94, 99
